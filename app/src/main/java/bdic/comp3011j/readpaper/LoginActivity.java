1ab5abd33324dc59daed247a1b1e3b0e9d55c566
LiYanTao <641217688tly@gmail.com>
2023-12-07 21:12:51 +0800

    remove login vpn proxy input

--- app/src/main/java/bdic/comp3011j/readpaper/LoginActivity.java
+++ app/src/main/java/bdic/comp3011j/readpaper/LoginActivity.java
@@ -23,10 +23,10 @@ import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.LogInListener;
 import cn.bmob.v3.listener.SaveListener;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     private SharedPreferences sharedPreferences;
     private RadioGroup rgLoginType;
-    private EditText etEmail, etPassword, etProxyPort;
+    private EditText etEmail, etPassword;
     private Button btnLogin,btnGoToRegister;
 
 
@@ -39,13 +39,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         rgLoginType = findViewById(R.id.rgLoginType);
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
-        etProxyPort = findViewById(R.id.etProxyPort);
         btnLogin = findViewById(R.id.btnLogin);
         btnGoToRegister = findViewById(R.id.btnGoToRegister);
         // 注册按钮监听事件
         btnLogin.setOnClickListener(this);
         btnGoToRegister.setOnClickListener(this);
-        rgLoginType.setOnCheckedChangeListener(this);
 
         loadUserInfo();
     }
@@ -54,26 +52,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         String email = sharedPreferences.getString("email", "");
         String password = sharedPreferences.getString("password", "");
         String device = sharedPreferences.getString("device", "Physical");
-        String proxyPort = sharedPreferences.getString("proxyPort", "");
 
         etEmail.setText(email);
         etPassword.setText(password);
 
         if ("Virtual".equals(device)) {
             rgLoginType.check(R.id.rbVirtualDevice);
-            etProxyPort.setText(proxyPort);
-            etProxyPort.setVisibility(View.VISIBLE);
         } else {
             rgLoginType.check(R.id.rbPhysicalDevice);
-        }
-    }
-
-    @Override
-    public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-        if (checkedId == R.id.rbVirtualDevice) {
-            etProxyPort.setVisibility(View.VISIBLE);
-        } else {
-            etProxyPort.setVisibility(View.GONE);
         }
     }
 
@@ -114,7 +100,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         int checkedRadioButtonId = rgLoginType.getCheckedRadioButtonId();
         if (checkedRadioButtonId == R.id.rbVirtualDevice) {
             editor.putString("device", "Virtual");
-            editor.putString("proxyPort", etProxyPort.getText().toString().trim());
         } else {
             editor.putString("device", "Physical");
         }
